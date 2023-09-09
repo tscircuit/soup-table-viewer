2df399c7f0b932363af11ead8916f8b9de7378da
seveibar <seveibar@gmail.com>
2023-09-08 22:37:01 -0700

    wip

--- src/SoupTableViewer.tsx
+++ src/SoupTableViewer.tsx
@@ -33,24 +33,69 @@ export const SoupTableViewer = ({ elements }: { elements: AnyElement[] }) => {
     }
   })
 
+  const elements3 = elements2.map((e) => {
+    let selector_path
+
+    const getSelectorPath = (e2) => {
+      const parent_key = Object.keys(e2.other_ids).find((k) =>
+        k.startsWith("source_")
+      )
+      if (!parent_key) return `.${e2.name}`
+      const parent_type = parent_key.slice(0, -3) // trim "_id"
+
+      const parent = elements2.find(
+        (p) =>
+          "type" in p &&
+          p.type === parent_type &&
+          p[parent_key] === e2[parent_key]
+      )
+
+      if (!parent) return `.${e2.name}`
+      // throw new Error(`Couldn't find specified parent: ${e2[parent_key]}`)
+
+      if (!("name" in parent)) return `#${parent[parent_key]} > .${e2.name}`
+
+      return `${getSelectorPath(parent)} > .${e2.name}`
+    }
+
+    if ("name" in e) {
+      selector_path = getSelectorPath(e)
+    }
+
+    return {
+      ...e,
+      selector_path,
+    }
+  })
+
   const columns: Column<any, any>[] = [
     { key: "primary_id", name: "primary_id" },
     {
       key: "type",
       name: "type",
-      // filterRenderer: (p) => (
-      //   <select onChange={(e) => p.onChange(e.target.value)}>
-      //     {element_types.map((type) => (
-      //       <option key={type} value={type}>
-      //         {type}
-      //       </option>
-      //     ))}
-      //   </select>
-      // ),
+      renderHeaderCell: (p) => (
+        <div style={{ lineHeight: 1.5, padding: 0 }}>
+          <div style={{ fontWeight: "bold" }}>{p.column.name}</div>
+          <hr />
+          <select
+          // onChange={(e) => p.onChange(e.target.value)}
+          >
+            {element_types.map((type) => (
+              <option key={type} value={type}>
+                {type}
+              </option>
+            ))}
+          </select>
+        </div>
+      ),
     },
     {
       key: "name",
       name: "name",
+    },
+    {
+      key: "selector_path",
+      name: "selector_path",
     },
     {
       key: "other_ids",
@@ -84,5 +129,12 @@ export const SoupTableViewer = ({ elements }: { elements: AnyElement[] }) => {
   //   })
   // })
 
-  return <ReactDataGrid columns={columns} rows={elements2} />
+  return (
+    <ReactDataGrid
+      style={{ height: 1000 }}
+      headerRowHeight={70}
+      columns={columns}
+      rows={elements3}
+    />
+  )
 }
